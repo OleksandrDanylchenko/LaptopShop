a4168a7edec4aa4a923d4674635a8f85d03cff5e
Alexander Danylchenko <olexander.danilchenko@gmail.com>
2020-02-23 20:08:25 +0200

    Added navigation bar

--- src/main/java/ua/alexd/controller/MainController.java
+++ src/main/java/ua/alexd/controller/MainController.java
@@ -6,19 +6,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.Collections;
-
 @Controller
 @RequestMapping("/")
 public class MainController {
     @GetMapping
     public String landing(@NotNull Model model) {
-        var controllers = new ArrayList<String>();
-        Collections.addAll(controllers, "availability", "basket", "buying", "client", "cpu", "display",
-                "employee", "gpu", "hardware", "hdd", "laptop", "label", "ram", "shop", "ssd", "type");
-        model.addAttribute("controllers", controllers);
         return "landing";
     }
-    // TODO Security layers for unique fields
 }
